fd9e1e65ee7e3835924acb718bfa640ed048dbe2
radheysm <radheysaraan@gmail.com>
2020-04-06 12:57:51 +0530

    Details component created

--- mcart/src/components/Details.js
+++ mcart/src/components/Details.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
-export default class Product extends Component {
+export default class Details extends Component {
     render() {
         return (
             <div>
                 <h3>
-                    Hello from product
+                    Hello from details
                 </h3>
             </div>
         )
--- mcart/src/components/Productlist.js
+++ mcart/src/components/Productlist.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Product from './Product'
 
 export default class Productlist extends Component {
     render() {
         return (
             <div>
-                <h3>Hello from Product List</h3>
+                <Product/>
             </div>
         )
     }
