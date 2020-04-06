e741a9941c85beedd746cfad6cd836a8374c287e
radheysm <radheysaraan@gmail.com>
2020-04-06 12:48:01 +0530

    Productlist Component created

---
+++ mcart/src/components/Productlist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Productlist extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product List</h3>
+            </div>
+        )
+    }
+}
